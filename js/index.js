f9bfb042738ae2e82b9e0af048c89ebe1fbec59f
whoisaarun <98503534+AaronDripYT@users.noreply.github.com>
2024-03-27 19:07:16 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
-var menu_clicked = false
+var menu_clicked = false;
+var width = window.innerWidth;
+
+window.addEventListener('load', (event) => {
+    var menu = document.getElementById('menu');
+    var footerTxt = document.getElementById('footerTxt');
+
+    if (width >= 850){
+        menu.style.transform = 'scale(0.97)';
+        menu.style.display = 'none';
+        menu_clicked = false;
+        footerTxt.innerHTML = `Open school hours | <span onclick="navigator.clipboard.writeText(''); navigator.clipboard.writeText(this.innerHTML); alert('Phone number copied.')">+1 (246) 367-8180</span> | <a target="_blank" href="mailto:campfitness246@gmail.com">campfitness246@gmail.com</a>`;
+    } else{
+        footerTxt.innerHTML = `Open school hours | <span onclick="navigator.clipboard.writeText(''); navigator.clipboard.writeText(this.innerHTML); alert('Phone number copied.')">+1 (246) 367-8180</span>  <a target="_blank" href="mailto:campfitness246@gmail.com">campfitness246@gmail.com</a>`;
+    }
+});
 
 function menu(btn){
-    var menu = document.getElementById('menu')
+    var menu = document.getElementById('menu');
 
     if (menu_clicked == false){
         menu.style.display = 'block';
@@ -18,14 +33,17 @@ function menu(btn){
     }
 };
 
-window.addEventListener('resize', function () {
-    console.log(window.innerWidth)
-
+window.addEventListener('resize', function (){
     if (window.innerWidth >= 850) {
-        var menu = document.getElementById('menu')
+        var menu = document.getElementById('menu');
+        var footerTxt = document.getElementById('footerTxt');
 
         menu.style.transform = 'scale(0.97)';
         menu.style.display = 'none';
         menu_clicked = false;
+        footerTxt.innerHTML = `Open school hours | <span onclick="navigator.clipboard.writeText(''); navigator.clipboard.writeText(this.innerHTML); alert('Phone number copied.')">+1 (246) 367-8180</span> | <a target="_blank" href="mailto:campfitness246@gmail.com">campfitness246@gmail.com</a>`;
+    } else{
+        var footerTxt = document.getElementById('footerTxt');
+        footerTxt.innerHTML = `Open school hours | <span onclick="navigator.clipboard.writeText(''); navigator.clipboard.writeText(this.innerHTML); alert('Phone number copied.')">+1 (246) 367-8180</span>  <a target="_blank" href="mailto:campfitness246@gmail.com">campfitness246@gmail.com</a>`;
     }
 });
